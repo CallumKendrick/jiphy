8c79ba74fdd7ff737cb7c487c80fa4c09bd2b7b6
Aaron Tello-Wharton <aaron.tkd@hotmail.com>
2017-02-26 06:31:25 +0000

    made captions more path friendly

--- converters/cache_captions.sh
+++ converters/cache_captions.sh
@@ -10,14 +10,14 @@ nodejs index-caption.js "/caption/inception-yify-english.srt" "inception.mkv"
 nodejs index-caption.js "/caption/iron-man-yify-english.srt" "iron_man.avi"
 nodejs index-caption.js "/caption/Kung.Fu.Panda.2008.720p.BrRip.x264.YIFY.srt" "kung_fu_panda.avi"
 nodejs index-caption.js "/caption/Pirates.of.the.Caribbean.Curse.of.the.Black.Pearl.2003.720p.BrRip.x264.Deceit.YIFY.srt" "pirates_one.avi"
-nodejs index-caption.js "/caption/PiratesOfTheCaribbeanDeadMan'sChest.brrip.720p.2006.srt" "pirates_two.avi"
-nodejs index-caption.js "/caption/At World's End [Bluray 720p and 1080p SRT].srt" "pirates_three.avi"
+nodejs index-caption.js "/caption/dmc.srt" "pirates_two.avi"
+nodejs index-caption.js "/caption/awe.srt" "pirates_three.avi"
 nodejs index-caption.js "/caption/Scott.Pilgrim.Vs.The.World.2010.720p.BRRip.x264-HDLiTE.Hi.srt" "scott_pilgrim.mp4"
 nodejs index-caption.js "/caption/shaun-of-the-dead-yify-english.srt" "shaun_of_the_dead.mkv"
 nodejs index-caption.js "/caption/the-social-network-yify-english.srt" "social_network.mkv"
-nodejs index-caption.js "/caption/The SpongeBob SquarePants Movie [2004]-720p-BRrip-x264.srt" "spongebob.mp4"
+nodejs index-caption.js "/caption/spongebob.srt" "spongebob.mp4"
 nodejs index-caption.js "/caption/step-brothers-yify-english.srt" "step_brothers.avi"
-nodejs index-caption.js "/caption/Thor.2011.1080p.BrRip.x264.YIFY+HI.srt" "thor.avi"
+nodejs index-caption.js "/caption/thor.srt" "thor.avi"
 
 
 
