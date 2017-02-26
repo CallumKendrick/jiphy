f9606ad7b88843051400e12df9fb7e44cdb6bcc9
Aaron Tello-Wharton <aaron.tkd@hotmail.com>
2017-02-26 05:30:00 +0000

    added caption caching, some bugs but it seems most captions work

---
+++ converters/cache_captions.sh
@@ -0,0 +1,53 @@
+#!/bin/sh
+
+#/elasticsearch/bin/elasticsearch
+
+cd /admin-scripts
+nodejs index-caption.js "/caption/the-dark-knight-yify-english.srt" "batman_tdk.mp4"
+nodejs index-caption.js "/caption/the-dark-knight-rises-yify-english.srt" "batman_tdkr.mkv"
+nodejs index-caption.js "/caption/the-emperors-new-groove.720p.BluRay.x264.YIFY.srt" "emperors_grove.avi"
+nodejs index-caption.js "/caption/inception-yify-english.srt" "inception.mkv"
+nodejs index-caption.js "/caption/iron-man-yify-english.srt" "iron_man.avi"
+nodejs index-caption.js "/caption/Kung.Fu.Panda.2008.720p.BrRip.x264.YIFY.srt" "kung_fu_panda.avi"
+nodejs index-caption.js "/caption/Pirates.of.the.Caribbean.Curse.of.the.Black.Pearl.2003.720p.BrRip.x264.Deceit.YIFY.srt" "pirates_one.avi"
+nodejs index-caption.js "/caption/PiratesOfTheCaribbeanDeadMan'sChest.brrip.720p.2006.srt" "pirates_two.avi"
+nodejs index-caption.js "/caption/At World's End [Bluray 720p and 1080p SRT].srt" "pirates_three.avi"
+nodejs index-caption.js "/caption/Scott.Pilgrim.Vs.The.World.2010.720p.BRRip.x264-HDLiTE.Hi.srt" "scott_pilgrim.mp4"
+nodejs index-caption.js "/caption/shaun-of-the-dead-yify-english.srt" "shaun_of_the_dead.mkv"
+nodejs index-caption.js "/caption/the-social-network-yify-english.srt" "social_network.mkv"
+nodejs index-caption.js "/caption/The SpongeBob SquarePants Movie [2004]-720p-BRrip-x264.srt" "spongebob.mp4"
+nodejs index-caption.js "/caption/step-brothers-yify-english.srt" "step_brothers.avi"
+nodejs index-caption.js "/caption/Thor.2011.1080p.BrRip.x264.YIFY+HI.srt" "thor.avi"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
