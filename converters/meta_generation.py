862d46377664bbc4b60266661541ccfdbbcaddb9
Aaron Tello-Wharton <aaron.tkd@hotmail.com>
2017-02-26 02:19:38 +0000

    finished meta_generation.py

--- converters/meta_generation.py
+++ converters/meta_generation.py
@@ -1,11 +1,16 @@
 from os import listdir
 from os.path import isfile, join
+import glob
 import subprocess
 
 
 def getLength(filename):
   result = float(subprocess.check_output(['ffprobe', '-i', filename, '-show_entries', 'format=duration', '-v', 'quiet', '-of', 'csv=%s' % ("p=0")])) * 1000
   return result
+
+def frameCount(filename):
+	path = '/frames/' + filename
+	return len(glob.glob1(path,"*.jpg"))
 
 
 mypath = "../"
@@ -16,7 +21,9 @@ for filename in onlyfiles:
 	file = open("/frames/" + filename + "/META", "w")
 
 	print(getLength(mypath + filename))
-	file.write("duration : " + str(getLength(mypath + filename)))
+	file.write("duration : " + str(getLength(mypath + filename)) + "\n" ) 
+	print(frameCount(filename))
+	file.write("framecount : " + str(frameCount(filename)))
 
 	file.close()
 
