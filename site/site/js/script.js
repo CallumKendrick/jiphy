324b7f6eb1c56a6a742f860422aa4123da998b34
Zattri <daninjakidy@gmail.com>
2017-03-01 19:44:47 +0000

    Added more comments, tidied code, added in webcam preview shut off button, fixed caption issue

--- site/site/js/script.js
+++ site/site/js/script.js
@@ -1,43 +1,49 @@
 var fetchedCap;
 var fetchArr;
+var video = document.getElementById("videoElement");
 
 function makeImgArray(imageArray, caption) {
     fetchedCap = caption;
     fetchedArr = imageArray;
-    console.log("FETCHED Caption: " + fetchedCap + "ImageArr: " + fetchedArr);
+
+    // Fetched caption log
+    console.log("[FETCHED] Caption: " + fetchedCap + "ImageArr: " + fetchedArr);
     }
 
 function makeGif(type){
     var text;
     var time;
     var frames;
-    if (fetchedCap == "") {
-        var text = document.getElementById('text').value;
-    }
-    else {
-        text = fetchedCap;
-    }
-    
+
+    // Fetch vars from front-end UI
     var height = document.getElementById("height").value;
     var width = document.getElementById("width").value;
     var searchTerm = document.getElementById("search").value;
     var comboVal = document.getElementById("combo").value;
 
-    console.log(height, width);
 
+    // Overwrites fetchedCap with text input if there is any, or caption is null
+    if (fetchedCap == "" | document.getElementById('text').value != "") {
+        text = document.getElementById('text').value;
+    }
+    else {
+        text = fetchedCap;
+    }
+
+    // Catch for if height and width = nothing - Set to 300px
     if (height == '' || height == null || isNaN(height)) { var height = 300; }
     if (width == '' || width == null || isNaN(width)) { var width = 300; }
 
-    if (text == null || text == "") {
-        text = "";
-    }
-
+    // Declare the time as the value of the combo box that dictates seconds
     time = comboVal;
+    // Frames are recorded every 0.1 of a second
     frames = comboVal * 10;
 
 
     if (type == "webcam") {
-        console.log("GIF LOG | " + height + "x" + width + "px - Time:" + time + " Frames:" + frames);
+        // Input log
+        console.log("[GIF LOG] " + height + "x" + width + "px - Time:" + time + " Frames:" + frames + " Caption: " + text);
+
         gifshot.createGIF({'interval': 0.05, 'gifHeight': height, 'gifWidth': width, 'text': text, 'fontSize': sizeFont(height, width),
         'time': time, 'numFrames': frames, 'keepCameraOn': false},
         function(obj) {
@@ -46,26 +52,35 @@ function makeGif(type){
                 animatedImage = document.createElement('img');
                 animatedImage.src = image;
                 var getGif = document.getElementById("gif");
+                // If there is another GIF already displayed remove it
                 if (getGif.childNodes[0] != null) {
                     getGif.removeChild(getGif.childNodes[0]);
                 }
                 document.getElementById("gif").appendChild(animatedImage);
+                // Clear text box on completion
+                document.getElementById('text').value = "";
             }
         });
     }
 
     else if (type == "image") {
-        gifshot.createGIF({'images': fetchedArr, 'text': fetchedCap, 'interval': 0.2, 'gifHeight': height, 'gifWidth': width, 'fontSize': sizeFont(height, width)},
+        // Input log
+        console.log("[GIF LOG] " + height + "x" + width + "px - Time:" + time + " Frames:" + frames + " Caption: " + text);
+
+        gifshot.createGIF({'images': fetchedArr, 'text': text, 'interval': 0.2, 'gifHeight': height, 'gifWidth': width, 'fontSize': sizeFont(height, width)},
             function(obj) {
                 if(!obj.error) {
                     var image = obj.image,
                     animatedImage = document.createElement('img');
                     animatedImage.src = image;
                     var getGif = document.getElementById("gif");
+                    // If there is another GIF already displayed remove it
                     if (getGif.childNodes[0] != null) {
                         getGif.removeChild(getGif.childNodes[0]);
                     }
                     document.getElementById("gif").appendChild(animatedImage);
+                    // Clear text box on completion
+                    document.getElementById('text').value = "";
                 }
             });
     }
@@ -83,25 +98,30 @@ function sizeFont(h, w) {
     return font;
 }
 
+// -------------------------------------------------
+// WEBCAM FUNCTIONS
+// -------------------------------------------------
 
-// TO DO
-// Make the ability to have an images array
-// Make webcam and images GIF buttons work
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
+window.onLoad = initVideo();
+
+function initVideo() {
+    navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia || navigator.oGetUserMedia;
+    if (navigator.getUserMedia) {
+        navigator.getUserMedia({video: true}, handleVideo, videoError);
+    }
+
+    function handleVideo(stream) {
+        video.src = window.URL.createObjectURL(stream);
+    }
+
+    function videoError(e) {
+        console.log("Video stream denied or unsupported");
+    }
+}
+
+// Closes the webcam preview, but doesn't disable camera
+function closePreview() {
+    video.pause();
+    video.src = "";
+    console.log("[WEBCAM] - Preview terminated");
+}
